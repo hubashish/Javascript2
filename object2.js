a753cbeb4bd58b1b622a943b96b415f46f90afd5
Nagesh Giri <140241817+hubashish@users.noreply.github.com>
2025-01-12 12:15:29 +0000

    object2

---
+++ object2.js
@@ -0,0 +1,27 @@
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Radhe"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "something@gmail.com",
+    fullname:{
+        userfullname: {
+            firstname: "Nagesh",
+            lastname: "Giri"
+        }
+    }
+ }
+
+
+// console.log(regularUser.fullname)
+
+const obj1 = { 1:"a",2:"b"}
+const obj2 = { 3:"c",4:"d"}
+const obj3 = { 5:"e",6:"f"}
+const obj4 = { 7:"g",8:"h"}
+
+console.log(obj3);
