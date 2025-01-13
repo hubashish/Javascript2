b286ac595859c49a689cb88c63ddf74d80f47ebc
Nagesh Giri <140241817+hubashish@users.noreply.github.com>
2025-01-13 16:43:01 +0000

    object3

---
+++ object3.js
@@ -0,0 +1,10 @@
+const course = {
+    coursename : "JS in Hindi",
+    price: "555",
+    courseInstructor:"Nagesh"
+}
+
+const {courseInstructor : instructor} = course
+
+console.log(instructor);
+
