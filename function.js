81fdf7d1b2c84337df1b48c0f831cf54fb14cc3a
Nagesh Giri <140241817+hubashish@users.noreply.github.com>
2025-01-13 16:57:41 +0000

    function

---
+++ function.js
@@ -0,0 +1,30 @@
+console.log("N");
+console.log("A");
+console.log("G");
+console.log("E");
+console.log("S");
+console.log("H");
+
+
+function sayMyName(){
+console.log("N");
+console.log("A");
+console.log("G");
+console.log("E");
+console.log("S");
+console.log("H");
+}
+sayMyName()
+
+function addTwoNumbers(number1,number2){
+    console.log(number1+number2);
+}
+addTwoNumbers(3 ,null)
+
+function addTwoNumber(number1 , number2){
+    let result = number1+number2
+    return result
+}
+const result= addTwoNumber(3,5)
+
+console.log("Result:" , result);
