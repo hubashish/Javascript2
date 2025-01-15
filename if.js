fc34468effd97d8436e539c0bf8f81c6282b44f0
Nagesh Giri <140241817+hubashish@users.noreply.github.com>
2025-01-15 16:30:25 +0000

    iife

---
+++ if.js
@@ -0,0 +1,13 @@
+// immediatly invoked function expressions (IIFE)
+
+
+
+(function chai (){
+    console.log('DB CONNECTED');
+
+})();
+ ((name)=>{
+    console.log('DB CONNECTED TWO ${name}');
+
+ })('Nagesh')
+
