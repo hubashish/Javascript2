388c6982d8ab69aab3a0450ad8a38b99a75f7d87
Nagesh Giri <140241817+hubashish@users.noreply.github.com>
2025-01-16 16:14:06 +0000

    control

---
+++ control.js
@@ -0,0 +1,42 @@
+//controlflow
+// if
+// operator
+
+// normal check
+
+// const isUserlogggedIn =true
+
+// if(2 == "2"){
+//     console.log("executed");
+// }
+
+// strictly check
+// const isUserlogggedIn =true
+
+// if(5 === "5"){
+//     console.log("executed");
+// }
+
+// const isUserlogggedIn =true
+// const temperature = 41
+
+// if(temperature === 58){
+//     console.log("less tham 50");
+// }
+// else{
+
+// console.log("temeprature is greater than 50");
+// }
+
+// console.log("Execute");
+
+
+const score =200
+
+if (score> 100) {
+    let power = "fly"
+    console.log('User power: ${power}');
+
+}
+console.log('User power: ${power}');
+
