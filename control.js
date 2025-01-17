21ce0d5501f9211ceac1c19728d1ee6ae9a32c55
Nagesh Giri <140241817+hubashish@users.noreply.github.com>
2025-01-17 15:20:39 +0000

    update

--- control.js
+++ control.js
@@ -31,17 +31,43 @@
 // console.log("Execute");
 
 
-const score =200
+// const score =200
 
-if (score> 100) {
-    let power = "fly"
-    console.log('User power: ${power}');
+// if (score> 100) {
+//     let power = "fly"
+//     console.log('User power: ${power}');
 
+// }
+// console.log('User power: ${power}');
+
+// // shorthand notation
+
+//  const balance = 1000
+// if (balance > 500) console.log("test"),
+// console.log("test2")
+
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750){
+//     console.log("less than 750");
+
+// } else if (balance < 900 ){
+//     console.log("less than 900");
+
+// } else {
+//     console.log("less than 1200");
+// }
+
+const isUserloggedIn  = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (isUserloggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course") ;
 }
-console.log('User power: ${power}');
 
-// shorthand notation
-
-const balance = 1000
-if (balance > 500) console.log("test"),
-console.log("test2")
+if(loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
