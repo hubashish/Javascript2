ffcf6e3d1e3e01c8387cb8e0fc8407672eb698f1
Nagesh Giri <140241817+hubashish@users.noreply.github.com>
2025-01-18 16:41:29 +0000

    loops2

---
+++ loops2.js
@@ -0,0 +1,21 @@
+let index = 0
+
+while (2==2){
+    console.log('Value of index is ${index}');
+    index = index + 2
+
+}
+
+let myArray = ['dlash','batman','superman']
+
+let arr = 0
+while (arr< myArray.length){
+    console.log('Value is ${myArray[arr]}');
+    arr = arr+1
+}
+let score = 1
+
+do{
+    console.log('Score is ${score}');
+    score++
+} while (score <=10);
