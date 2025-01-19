291da80ebc0fe6202088ec8eb2fc9108cefb0cfc
Nagesh Giri <140241817+hubashish@users.noreply.github.com>
2025-01-19 08:00:39 +0000

    loop4

---
+++ loop4.js
@@ -0,0 +1,22 @@
+const myObject = {
+    'game1': 'NFS',
+    'game2': 'Spiderman',
+    'game3': 'Temple run '
+}
+
+for (const key in myObject){
+    console.log('${key} shortcut is for ${myObject[key]}');
+}
+const programming = ["TempleRUn","Spiderman","Clash OF Clan","BGMI"]
+
+for (const key in programming) {
+    console.log(programming[key]);
+}
+
+
+const coding =["js","ruby","pyhton","cpp"]
+
+coding.forEach(function (val){
+    console.log(CSSStyleValue)
+})
+
